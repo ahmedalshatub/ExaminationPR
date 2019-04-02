3a0c4b6f8d395c6881a712754558f4ada57d1623
ahmed.alshatub <ahmed.alshatub@yahoo.com>
2019-04-02 23:52:49 +0300

    newpr

--- examdata (1).sql
+++ examdata (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 25, 2019 at 12:53 AM
+-- Generation Time: Mar 20, 2019 at 05:08 PM
 -- Server version: 10.1.29-MariaDB
 -- PHP Version: 7.2.0
 
@@ -19,7 +19,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `ExamData`
+-- Database: `examdata`
 --
 
 -- --------------------------------------------------------
@@ -30,11 +30,18 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `examtable` (
   `ExamID` int(11) NOT NULL,
-  `Examname` text NOT NULL,
+  `ExamTitle` text NOT NULL,
   `MetalID` int(11) NOT NULL,
   `ExamDate` date NOT NULL,
   `ExamTime` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `examtable`
+--
+
+INSERT INTO `examtable` (`ExamID`, `ExamTitle`, `MetalID`, `ExamDate`, `ExamTime`) VALUES
+(5, 'First exam', 1, '2019-03-20', '12:30');
 
 -- --------------------------------------------------------
 
@@ -49,6 +56,44 @@ CREATE TABLE `metaltable` (
   `Level` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `metaltable`
+--
+
+INSERT INTO `metaltable` (`MetalID`, `MetalName`, `TeacherID`, `Level`) VALUES
+(1, 'c++', 1, 1),
+(2, 'java', 1, 3);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `questionchoice`
+--
+
+CREATE TABLE `questionchoice` (
+  `ChID` int(11) NOT NULL,
+  `ChoiceTitle` text NOT NULL,
+  `QuestionID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `questionchoice`
+--
+
+INSERT INTO `questionchoice` (`ChID`, `ChoiceTitle`, `QuestionID`) VALUES
+(17, '1', 6),
+(18, '2', 6),
+(19, '3', 6),
+(20, '4', 6),
+(21, 'ali', 7),
+(22, 'ahmed', 7),
+(23, 'ahmed ali', 7),
+(24, 'ali ahmed', 7),
+(25, '|Ø§Ù„Ø³Ù„Ø§Ù… Ø¹Ù„ÙŠÙƒÙ…', 8),
+(26, 'Ø´Ù„ÙˆÙ†ÙƒÙ…', 8),
+(27, 'ÙˆØ¹Ù„ÙŠÙƒÙ… Ø§Ù„Ø³Ù„Ø§Ù…', 8),
+(28, 'Ø«ÙŠÙ…Ù„Ø§', 8);
+
 -- --------------------------------------------------------
 
 --
@@ -59,9 +104,19 @@ CREATE TABLE `questionstable` (
   `QuestionID` int(11) NOT NULL,
   `QuestionTittle` text NOT NULL,
   `QuestionType` text NOT NULL,
-  `QuestionDgree` int(11) NOT NULL,
   `ExamID` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `questionstable`
+--
+
+INSERT INTO `questionstable` (`QuestionID`, `QuestionTittle`, `QuestionType`, `ExamID`) VALUES
+(4, '5', 'Ù‡Ø°Ø§ Ø²Ø§ÙŠØ¯', 0),
+(5, '5', 'Ø§ÙƒØªØ¨ Ø§ÙŠ Ø´ÙŠ ØªØ¹Ø±ÙÙ‡ Ø¹Ù† Ø§Ù„c++', 0),
+(6, '5', 'chois any thing', 0),
+(7, '5', 'name', 0),
+(8, '5', 'hello', 0);
 
 -- --------------------------------------------------------
 
@@ -71,7 +126,8 @@ CREATE TABLE `questionstable` (
 
 CREATE TABLE `stdgreetable` (
   `StID` int(11) NOT NULL,
-  `ExamID` int(11) NOT NULL
+  `ExamID` int(11) NOT NULL,
+  `TheDgree` float NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -82,10 +138,19 @@ CREATE TABLE `stdgreetable` (
 
 CREATE TABLE `studentstable` (
   `StID` int(11) NOT NULL,
-  `StName` int(11) NOT NULL,
-  `StPassword` int(11) NOT NULL,
+  `StName` text NOT NULL,
+  `StPassword` text NOT NULL,
   `StLevel` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `studentstable`
+--
+
+INSERT INTO `studentstable` (`StID`, `StName`, `StPassword`, `StLevel`) VALUES
+(2, 'ahmed1', '1234', 2),
+(3, 'ali', '899', 2),
+(4, 'hhh', 'aaa', 1);
 
 -- --------------------------------------------------------
 
@@ -96,8 +161,19 @@ CREATE TABLE `studentstable` (
 CREATE TABLE `teacherstable` (
   `TeacherID` int(11) NOT NULL,
   `TeacherName` text NOT NULL,
-  `TeacherPassword` text NOT NULL
+  `TeacherPassword` text NOT NULL,
+  `Level` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `teacherstable`
+--
+
+INSERT INTO `teacherstable` (`TeacherID`, `TeacherName`, `TeacherPassword`, `Level`) VALUES
+(1, 'Ahmed', 'Hamada', 0),
+(3, 'Ali', 'hh', 0),
+(4, 'mo', '132', 0),
+(7, 'ccc', '222', 0);
 
 --
 -- Indexes for dumped tables
@@ -114,6 +190,12 @@ ALTER TABLE `examtable`
 --
 ALTER TABLE `metaltable`
   ADD PRIMARY KEY (`MetalID`);
+
+--
+-- Indexes for table `questionchoice`
+--
+ALTER TABLE `questionchoice`
+  ADD PRIMARY KEY (`ChID`);
 
 --
 -- Indexes for table `questionstable`
@@ -141,31 +223,37 @@ ALTER TABLE `teacherstable`
 -- AUTO_INCREMENT for table `examtable`
 --
 ALTER TABLE `examtable`
-  MODIFY `ExamID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `ExamID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `metaltable`
 --
 ALTER TABLE `metaltable`
-  MODIFY `MetalID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `MetalID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `questionchoice`
+--
+ALTER TABLE `questionchoice`
+  MODIFY `ChID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
 
 --
 -- AUTO_INCREMENT for table `questionstable`
 --
 ALTER TABLE `questionstable`
-  MODIFY `QuestionID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `QuestionID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `studentstable`
 --
 ALTER TABLE `studentstable`
-  MODIFY `StID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `StID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `teacherstable`
 --
 ALTER TABLE `teacherstable`
-  MODIFY `TeacherID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `TeacherID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
